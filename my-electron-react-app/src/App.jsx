0945dcd5aae48cf88a38b0998c7772b50ada8b30
alex0593 <aa69565@gmail.com>
2025-04-10 12:43:20 -0600

    Reloj y cronometro

--- my-electron-react-app/src/App.jsx
+++ my-electron-react-app/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import RelojElectronico from './Componentes/reloj/reloj'
+import Cronometro from './Componentes/cronometro/cronometro'
 
 function App() {
   return (
-    <h1 style={{ padding: '3rem', fontSize: '2rem' }}>¡Funciona Vite + React + Electron!</h1>
+    <div className="App">
+      <RelojElectronico />
+      <Cronometro />
+    </div>
   );
 }
-export default App;
 
+export default App;
