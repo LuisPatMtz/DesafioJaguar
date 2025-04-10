26f4bc1221febed22e819d09dce4c2c1c044e662
LuisPatMtz <luispatmartinez@gmail.com>
2025-04-09 20:40:04 -0600

    Primer commit del desadio jaguar

---
+++ my-electron-react-app/electron/main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path'); // <-- Esta línea es importante
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 700,
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  const isDev = process.env.NODE_ENV === 'development';
+
+  if (isDev) {
+    win.loadURL('http://localhost:5173');
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
---
+++ my-electron-react-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1 style={{ padding: '3rem', fontSize: '2rem' }}>¡Funciona Vite + React + Electron!</h1>
+  );
+}
+export default App;
+
